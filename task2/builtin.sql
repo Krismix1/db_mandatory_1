e5cabc9060f748fd1f588d238589eff432daca58
Epharas <cris0922@stud.kea.dk>
2019-03-18 21:38:25 +0100

    Add trigger for max books loan count

---
+++ task2/builtin.sql
@@ -0,0 +1,5 @@
+USE library;
+
+INSERT INTO borrower_types(name, max_books_count) VALUES ('Student', 3);
+INSERT INTO borrower_types(name, max_books_count) VALUES ('Teacher', 2);
+INSERT INTO borrower_types(name, max_books_count) VALUES ('Guest', 1);
--- task2/schema.sql
+++ task2/schema.sql
@@ -8,42 +8,43 @@ CREATE TABLE books (
   title                varchar(64) NOT NULL UNIQUE,
   authors              varchar(255), -- later could be changed to a FK
   publisher            varchar(64),
-  year_of_publication  tinyint,
+  year_of_publication  smallint unsigned,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE book_copies (
-  copy_number int unsigned NOT NULL AUTO_INCREMENT,
-  book_id int unsigned NOT NULL,
-  status varchar(16) NOT NULL,
-  allowed_loan_period tinyint NOT NULL,
+  copy_number          int unsigned NOT NULL AUTO_INCREMENT,
+  book_id              int unsigned NOT NULL,
+  status               enum('available', 'taken') NOT NULL,
+  allowed_loan_period  tinyint unsigned NOT NULL, -- unit of measurement: days
   PRIMARY KEY (copy_number),
   FOREIGN KEY (book_id) REFERENCES books(id)
 );
 
 CREATE TABLE borrower_types (
-  id          int unsigned NOT NULL AUTO_INCREMENT,
-  name        varchar(16) NOT NULL UNIQUE, -- not empty
-  max_number  tinyint NOT NULL, -- > 0?
+  id               int unsigned NOT NULL AUTO_INCREMENT,
+  name             varchar(16) NOT NULL UNIQUE, -- not empty
+  max_books_count  tinyint unsigned NOT NULL, -- > 0?
   PRIMARY KEY (id)
 );
 
 CREATE TABLE borrowers (
-  id int unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(64) NOT NULL UNIQUE,
-  mail_address varchar(128) NOT NULL UNIQUE,
-  borrower_type_id int unsigned NOT NULL,
+  id                int unsigned NOT NULL AUTO_INCREMENT,
+  name              varchar(64) NOT NULL UNIQUE,
+  mail_address      varchar(128) NOT NULL UNIQUE,
+  borrower_type_id  int unsigned NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (borrower_type_id) REFERENCES borrower_types(id)
 );
 
 CREATE TABLE loans (
-  id int unsigned NOT NULL AUTO_INCREMENT,
-  borrower_id int unsigned NOT NULL,
-  book_id int unsigned NOT NULL,
-  loaned_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  returned_at timestamp NOT NULL,
+  id                int unsigned NOT NULL AUTO_INCREMENT,
+  loan_period       tinyint unsigned NOT NULL, -- unit of measurement: days
+  borrower_id       int unsigned NOT NULL,
+  book_copy_number  int unsigned NOT NULL,
+  loaned_at         timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  returned_at       timestamp NULL DEFAULT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (borrower_id) REFERENCES borrowers(id),
-  FOREIGN KEY (book_id) REFERENCES books(id)
+  FOREIGN KEY (book_copy_number) REFERENCES book_copies(copy_number)
 );
